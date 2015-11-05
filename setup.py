eedf069250126789f418406c268c8325ff32384c
sergey@telminov.ru <sergey@telminov.ru>
2015-11-05 19:17:48 +0300

    distribution by docker

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 from distutils.core import setup
 
 setup(
-    name='django-park-worker-p3',
+    name='park-worker-p3',
     version='0.0.1',
     description='Workers for park-keeper project for python version 3.',
     author='Telminov Sergey',
     url='https://github.com/telminov/park-worker-p3',
-    packages=['parkworker3',],
+    packages=[
+        'parkworker3',
+        'parkworker3/bin',
+        'parkworker3/monits',
+    ],
     license='The MIT License',
     install_requires=[
         'park-worker-base', 'pyzmq', 'pytz', 'aiohttp'
