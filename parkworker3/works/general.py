ebe115535bbb3a33c858d09cbc2261e4c22a4729
sergey@telminov.ru <sergey@telminov.ru>
2015-12-03 16:28:52 +0300

    add support of adapted for park-keeper commands

--- parkworker3/works/general.py
+++ parkworker3/works/general.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import json
+from bson import json_util
 import subprocess
 import asyncio
 from parkworker import const
@@ -10,7 +12,10 @@ class LocalCommandWork(AsyncWork):
     name = 'general.local_command'
     description = 'Local command work. Options: \n' \
                   ' - command. Script for running. \n' \
-                  ' - args. Optional. Array with arguments for command. \n'
+                  ' - args. Optional. Array with arguments for command. \n' \
+                  ' - is_adapted. Optional. Is output of script adapted for park-keeper ' \
+                  '(use TaskResult.get_json() output format). \n' \
+                  ' - cwd. Optional. Set current working directory.'
 
     async def async_work(self, **kwargs):
         command = kwargs['command']
@@ -20,6 +25,7 @@ class LocalCommandWork(AsyncWork):
             command, *args,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
+            cwd=kwargs.get('cwd'),
         )
 
         await process.wait()
@@ -30,9 +36,15 @@ class LocalCommandWork(AsyncWork):
             level = const.LEVEL_FAIL
 
         stdout = await process.stdout.read()
-        check_result = TaskResult(
-            level=level,
-            extra={'stdout': stdout.decode('utf-8')},
-        )
+
+        if kwargs.get('is_adapted') and level == const.LEVEL_OK:
+            parsed_stdout = json.loads(stdout.decode('utf-8'), object_hook=json_util.object_hook)
+            check_result = TaskResult(**parsed_stdout)
+
+        else:
+            check_result = TaskResult(
+                level=level,
+                extra={'stdout': stdout.decode('utf-8')},
+            )
 
         return check_result
